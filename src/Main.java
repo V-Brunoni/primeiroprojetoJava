e2d0a7f1355b4014f183c80270aaf9c8b03b0e99
Vinicius Brunoni <viniciusbrunoni8@gmail.com>
2024-12-28 14:18:54 -0300

    Atualizando o codigo teste

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-       
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        System.out.printf("Olá Mundo!\n");
+
+        /*for (int i = 1; i <= 10; i++) {
             System.out.println("i = " + i);
-        }
+        }*/
+
+        int teste = 10;
+        System.out.println(teste);
     }
 }
+
+
